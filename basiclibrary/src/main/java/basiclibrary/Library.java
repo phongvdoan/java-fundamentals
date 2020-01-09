ef6cb9e2f4e1fe6171856b5470710ffebfc13895
Phong <phongdoan.dev@gmail.com>
2020-01-09 11:13:08 -0800

    Day 4 Lab complete

--- basiclibrary/src/main/java/basiclibrary/Library.java
+++ basiclibrary/src/main/java/basiclibrary/Library.java
@@ -4,6 +4,9 @@
 package basiclibrary;
 
 
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Library {
     public boolean someLibraryMethod() {
         return true;
@@ -48,4 +51,60 @@ public class Library {
         }
         return arrayofLowestAverage;
     }
+
+    public static String analyzingWeatherData(int[][] arrayOfArrays) {
+        HashSet<Integer> uniqueTemp = new HashSet<>();
+        int highestTemp = arrayOfArrays[0][0];
+        int lowestTemp = arrayOfArrays[0][0];
+        for (int[] array: arrayOfArrays) {
+            for(int index = 0; index < array.length; index++) {
+                uniqueTemp.add(array[index]);
+                if(array[index] < lowestTemp){
+                    lowestTemp = array[index];
+                }
+                if(array[index] > highestTemp) {
+                    highestTemp = array[index];
+                }
+            }
+        }
+
+        String result = "High: " + highestTemp + "\n";
+        result += "Low: " + lowestTemp + "\n";
+        for( int checkforTemp = lowestTemp; checkforTemp < highestTemp; checkforTemp++) {
+            //https://stackoverflow.com/questions/15730134/java-opposite-of-contains-does-not-contain
+            if(!uniqueTemp.contains(checkforTemp)) {
+                result += "Never saw temperature: " + checkforTemp + "\n";
+            }
+        }
+        return result;
+    }
+
+    public static String tally(List<String> votes) {
+
+        String winner = " ";
+
+        HashMap<String, Integer> totalVotes = new HashMap<>();
+        for(String name : votes){
+            totalVotes.put(name, 0);
+        }
+
+        for( String vote : votes) {
+            int nameWasSeenCount = totalVotes.get(vote);
+            totalVotes.put(vote, nameWasSeenCount + 1);
+        }
+
+        int mostVotes = Integer.MIN_VALUE;
+
+        //https://www.baeldung.com/java-iterate-map
+        for(HashMap.Entry<String, Integer> entry : totalVotes.entrySet()) {
+            if (entry.getValue() > mostVotes){
+                mostVotes = entry.getValue();
+                winner = entry.getKey();
+
+            }
+        }
+
+        return winner;
+    }
+
 }
